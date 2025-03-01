1bf6eb3d5b34c76291ab7b48680374bdf9697cdb
Marika <marika.koivukangas@outlook.com>
2025-03-01 20:56:41 +0200

    muutoksia

---
+++ ReactMonimuoto/omasivu/src/App.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+      <footer>
+          <p>&copy; 2025 My Single Page</p>
+      </footer>
+  );
+};
+
+const App = () => {
+  return (
+      <div>
+          <Footer />
+      </div>
+  );
+};
+
+export default App;
